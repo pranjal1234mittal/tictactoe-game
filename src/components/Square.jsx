50f2ccafcacdb9d6c746c94b6de79afdd57b6e60
Pranjal Mittal <114818997+pranjal1234mittal@users.noreply.github.com>
2023-03-28 13:29:19 +0530

    applied css for winners by adding variables and cvlassname in square.jsx

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,6 +1,13 @@
-const Square = ({ value, onClick }) => {
+const Square = ({ value, onClick, isWinningSquare }) => {
+  const colorClassName = value === 'X' ? 'text-green' : 'text-orange';
+  const winnerClassName = isWinningSquare ? 'winning' : '';
+
   return (
-    <button type="button" className="square" onClick={onClick}>
+    <button
+      type="button"
+      className={`square ${colorClassName} ${winnerClassName}`}
+      onClick={onClick}
+    >
       {value}
     </button>
   );
